93d9504e700b70b74c7749caa8ea1bcb8a651368
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-06-05 20:45:57 +0200

    looked at http module; how to learn server and routing

--- first-app/app.js
+++ first-app/app.js
@@ -22,14 +22,33 @@
 //     else console.log('result', files)
 // })
 
-const EventEmitter = require("events");
-const Logger = require('./logger')
-const logger = new Logger();
+// const EventEmitter = require("events");
+// const Logger = require('./logger')
+// const logger = new Logger();
 
-// register a listener
-logger.on("MessageLogged",function(e){
-    console.log('Message Listened', e);
+// // register a listener
+// logger.on("MessageLogged",function(e){
+//     console.log('Message Listened', e);
 
-})
+// })
 
-logger.log('message')
+// logger.log('message')
+
+const http = require('http');
+
+const server = http.createServer((req, res) =>{
+    if(req.url === "/"){
+        res.write("hello world");
+        res.end();
+    }
+    if(req.url === "/api/courses"){
+        res.write(JSON.stringify([1, 2, 3]));
+        res.end();
+    }
+});
+
+
+
+server.listen(3000);
+
+console.log("Listening on port 3000");
