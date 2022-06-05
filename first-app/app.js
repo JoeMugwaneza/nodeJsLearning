1578d4ca6a1ee30b6430298fb6ecb3e6dcde45f3
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-06-05 14:07:57 +0200

    continued to learn more about nodejs

--- first-app/app.js
+++ first-app/app.js
@@ -1,8 +1,23 @@
 // const sayHello = require("./logger.js")
 // sayHello();
 
-const path = require('path');
+// const path = require('path');
 
-let pathObj = path.parse(__filename);
+// let pathObj = path.parse(__filename);
 
-console.log(pathObj);
+// console.log(pathObj);
+
+// const os = require('os');
+// let freeMemory = os.freemem();
+// let totalMemory = os.totalmem();
+
+// console.log(`Total Memory: ${totalMemory},
+// Free Memory: ${freeMemory}
+// `);
+
+const fs = require('fs');
+
+fs.readdir("$",function(error, files){
+    if(error) console.log('Error', error)
+    else console.log('result', files)
+})
