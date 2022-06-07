60e903d1d4f00c3a94d8048fd50a7f1847d7cbe4
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-06-07 06:09:53 +0200

    looked at server module and used nodemon to have dynamic server

--- first-app/app.js
+++ first-app/app.js
@@ -59,8 +59,23 @@
 
 // person1.greeting();
 
-const Logger = require('./logger');
-const logger = new Logger();
-logger.on('message', data => console.log('Called Listener', data));
+// const Logger = require('./logger');
+// const logger = new Logger();
+// logger.on('message', data => console.log('Called Listener', data));
 
-logger.log("Hello world Mfk");
+// logger.log("Hello world Mfk");
+
+const htpp = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = htpp.createServer((req, res) =>{
+    if(req.url === "/"){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>Home</h1')
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
